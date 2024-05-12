837fdc98fbcfcff164b31b9d3c07a32935a50832
EugeMarion <eugeniamarion@hotmail.com>
2024-05-12 16:28:47 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleContent(contentId) {
+    var contenido = document.getElementById(contentId);
+    contenido.style.display = contenido.style.display === 'block' ? 'none' : 'block';
+}
+
+
