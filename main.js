1b032b006734a63a9288c95853ea44a934c50b09
EugeMarion <eugeniamarion@hotmail.com>
2024-05-14 12:32:45 -0300

    Actualizacion cambios

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ function toggleContent(contentId) {
     contenido.style.display = contenido.style.display === 'block' ? 'none' : 'block';
 }
 
-
+var tarjetas = document.querySelectorAll('.card');
+tarjetas.forEach(function (tarjeta) {
+    tarjeta.addEventListener('touchstart', function () {
+        // Alternar la clase 'tactil-activo' en la tarjeta tocada
+        tarjeta.classList.toggle('tactil-activo');
+    });
+});
