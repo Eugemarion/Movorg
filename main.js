862a7f8e67c7dd1519e426c01beb30f0e0285393
EugeMarion <eugeniamarion@hotmail.com>
2024-05-19 15:44:59 -0300

    actualizacion

--- main.js
+++ main.js
@@ -10,3 +10,35 @@ tarjetas.forEach(function (tarjeta) {
         tarjeta.classList.toggle('tactil-activo');
     });
 });
+
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+    // Limpiar mensajes de error anteriores
+    document.getElementById('nombreError').textContent = '';
+    document.getElementById('emailError').textContent = '';
+    document.getElementById('telefonoError').textContent = '';
+
+    // Validar campos
+    let valid = true;
+
+    const nombre = document.getElementById('nombre').value;
+    if (nombre.trim() === '') {
+        document.getElementById('nombreError').textContent = 'El nombre es obligatorio';
+        valid = false;
+    }
+
+    const email = document.getElementById('email').value;
+    if (email.trim() === '') {
+        document.getElementById('emailError').textContent = 'El correo electrónico es obligatorio';
+        valid = false;
+    }
+
+    const telefono = document.getElementById('mensaje').value;
+    if (telefono.trim() === '') {
+        document.getElementById('mensajeError').textContent = 'El mensaje es obligatorio';
+        valid = false;
+    }
+
+    if (!valid) {
+        event.preventDefault();
+    }
+});
